4484504763e94928527e2044f5d752036b68b3fa
ankylosaurus <cheehsiaoensamuela@gmail.com>
2026-08-05 07:51:02 +0800

    change gateway

--- scripts/Test-LoadBalancing.ps1
+++ scripts/Test-LoadBalancing.ps1
@@ -71,7 +71,7 @@ $observedRequests = ($distribution | Measure-Object -Property markedRequests -Su
 @(
     "startedUtc=$($started.ToString('o'))",
     "marker=$marker",
-    "gateway=https://kong-gateway-proxy.kong.svc.cluster.local/",
+    "gateway=https://kong-kong-proxy.kong.svc.cluster.local/",
     "requested=50",
     "observedInAccessLogs=$observedRequests",
     "distinctPhpBackends=$reachedBackends"
